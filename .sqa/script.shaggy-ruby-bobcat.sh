ad06fe7c1ce3ef97d1acaa5e4deebcddde76edad
orviz <orviz@ifca.unican.es>
2022-09-30 10:15:58 +0200

    Add JePL file structure

--- .sqa/script.shaggy-ruby-bobcat.sh
+++
@@ -1,4 +0,0 @@
-(
-cd github.com/indigo-dc/udocker &&
-    fuji.py -OID http://hdl.handle.net/10261/157765 --tool_endpoint http://localhost:1071/fuji/api/v1/evaluate -T oai_pmh -E http://digital.csic.es/dspace-oai/request
-)
